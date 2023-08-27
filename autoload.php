a2c27e1a399184e8a36690eaf055532515d43990
hatotank <hatotank@gmail.com>
2023-08-28 00:47:09 +0900

    update Twitter API v2 対応

--- autoload.php
+++ autoload.php
@@ -12,7 +12,7 @@ spl_autoload_register(function ($class) {
     $prefix = 'Abraham\\TwitterOAuth\\';
 
     // base directory for the namespace prefix
-    $base_dir = __DIR__ . '/twitteroauth/';
+    $base_dir = __DIR__ . '/twitteroauth/src/';
 
     // does the class use the namespace prefix?
     $len = strlen($prefix);
