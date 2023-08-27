0ed1a5af879297caa72c78b069c415d5bc5efb15
hatotank <hatotank@gmail.com>
2023-08-28 00:54:19 +0900

    Revert " update Twitter API v2 対応"
    This reverts commit a2c27e1a399184e8a36690eaf055532515d43990.

--- autoload.php
+++ autoload.php
@@ -12,7 +12,7 @@ spl_autoload_register(function ($class) {
     $prefix = 'Abraham\\TwitterOAuth\\';
 
     // base directory for the namespace prefix
-    $base_dir = __DIR__ . '/twitteroauth/src/';
+    $base_dir = __DIR__ . '/twitteroauth/';
 
     // does the class use the namespace prefix?
     $len = strlen($prefix);
